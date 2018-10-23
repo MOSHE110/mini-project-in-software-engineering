8386c55b00ca5e9101645b2b2fd74bd0964f3278
mosheco110 <mosheco110@gmail.com>
2018-10-23 18:26:24 +0300

    Point3d semi-done

--- src/main/primitives/Point3D.java
+++ src/main/primitives/Point3D.java
@@ -1,4 +1,43 @@
 package primitives;
 
-public class Point3D {
+import org.jetbrains.annotations.NotNull;
+
+public class Point3D extends  Point2D{
+
+    protected Coordinate _z;
+
+    /***************** Ctor **********************/
+    public Point3D(){super();_z=new Coordinate();};
+    public Point3D(@NotNull Coordinate x,@NotNull Coordinate y,@NotNull Coordinate z){super(x,y); _z=new Coordinate(z);};
+    public Point3D(@NotNull Point3D point3D;){
+        this._x=point3D.get_x();
+        this._y=point3D.get_y();
+        this._z=point3D.get_z();
+    };
+
+    /***************** Geter/Seters **********************/
+    public Coordinate get_z() { return new Coordinate(_z); }
+    public void set_z(@NotNull Coordinate z){this._z=new Coordinate(z);}
+/*************************************************
+ * FUNCTION
+ * compareTo()
+ * PARAMETERS
+ * Point3D point3D
+ * RETURN VALUE
+ * 1
+ * -1
+ * 0
+ * compere the distance's of this-Point3D and tha other Point3D from the origin
+ * MEANING
+ * if this_distance >other_distancereturn 1
+ * if distance_this <distance_other return -1
+ * if Point3D from the origin.
+ * if distances are equal but not same coordinates return 0
+ *
+ * SEE ALSO
+ * getDistanceFromOrigin(),
+ * *****************************************/
+public  int copareTo(@NotNull Point3D point3D){
+
+}
 }
---
+++ src/tests/primitives/Point2DTest.java
@@ -0,0 +1,90 @@
+package primitives;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static groovy.util.GroovyTestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class Point2DTest {
+
+
+    public class CoordinateTest {
+        private Coordinate c1,c2,c3,c4;
+
+        @BeforeEach
+        void setUp()
+        {
+            c1 = new Coordinate();
+            c2 = new Coordinate(5);
+            c3 = new Coordinate(10);
+            c4 = new Coordinate(-3);
+        }
+
+        @Test
+        void getPositionTest()
+        {
+            assertAll(
+                    () -> assertEquals(0, c1.get_Coordinate_Value()),
+                    () -> assertEquals(5, c2.get_Coordinate_Value()),
+                    () -> assertEquals(10, c3.get_Coordinate_Value()),
+                    () -> assertEquals(-3, c4.get_Coordinate_Value())
+            );
+        }
+
+        @Test
+        void setCoordinatePositionTest()
+        {
+            c1.setCoordinateValu_(3);
+            c2.setCoordinateValu_(4);
+
+            assertAll(
+                    () -> assertEquals(3, c1.get_Coordinate_Value()),
+                    () -> assertEquals(4, c2.get_Coordinate_Value())
+            );
+        }
+
+        @Test
+        void compareToTest()
+        {
+            assertTrue(c2.compareTo(c3) < 0);
+        }
+
+        @Test
+        void staticAddTest()
+        {
+            Coordinate r1 = Coordinate.add(c1,c2);
+            Coordinate r2 = Coordinate.add(c2,c4);
+            Coordinate r3 = Coordinate.add(c2,c3);
+
+            Coordinate e1 = new Coordinate(5);
+            Coordinate e2 = new Coordinate(2);
+            Coordinate e3 = new Coordinate(15);
+            assertAll(
+                    () -> assertTrue(r1.compareTo(e1) == 0),
+                    () -> assertTrue(r2.compareTo(e2) == 0),
+                    () -> assertTrue(r3.compareTo(e3) == 0)
+            );
+        }
+
+        @Test
+        void staticSubtractTest()
+        {
+            Coordinate r1 = Coordinate.subtract(c1,c2);
+            Coordinate r2 = Coordinate.subtract(c2,c4);
+            Coordinate r3 = Coordinate.subtract(c2,c3);
+
+            Coordinate e1 = new Coordinate(-15);
+            Coordinate e2 = new Coordinate(8);
+            Coordinate e3 = new Coordinate(-5);
+            assertAll(
+                    () -> assertTrue(r1.compareTo(e1) == 0),
+                    () -> assertTrue(r2.compareTo(e2) == 0),
+                    () -> assertTrue(r3.compareTo(e3) == 0)
+            );
+        }
+
+    }
+
+}
